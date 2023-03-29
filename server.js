1a9093f1c6922a3400037266f61bb10c0eeecfad
Teebu Philip <52789917+teebuphilip@users.noreply.github.com>
2023-03-29 14:42:33 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-const
-  express = require('express'),
-  serveStatic = require('serve-static'),
-  history = require('connect-history-api-fallback'),
-  port = process.env.PORT || 8080 
-
-const app = express()
-
-app.use(history())
-app.use(serveStatic(__dirname + '/dist/spa'))
-app.listen(port)
+const express = require('express');
+const history = require('connect-history-api-fallback');
+const app = express();
+app.use(history());
+app.use(express.static('dist'));
+app.get('/', (req, res) => {
+  res.sendFile('dist/index.html');
+});
+app.listen(8080, () => console.log('server started'));
